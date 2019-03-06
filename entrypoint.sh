6701c9d25bc83962c6cd7c7e2102c2fc5e1fd774
Sebastien HEYD <sebastien.heyd@free.fr>
2019-03-06 13:06:52 +0100

    Unique name for CA certificate

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [[ "$*" == "/bin/bash /app/start.sh" ]]; then
     if [ ! -f /etc/nginx/certs/ca.crt ]; then
         echo "=== Generating a new Certificate Authority -> ca.crt  ================"
     	openssl genrsa -out /etc/nginx/certs/ca.key 2048
-    	openssl req -x509 -new -nodes -key /etc/nginx/certs/ca.key -sha256 -days 7300 -subj "/CN=My Private CA" -out /etc/nginx/certs/ca.crt
+	openssl req -x509 -new -nodes -key /etc/nginx/certs/ca.key -sha256 -days 7300 -subj "/CN=Nginx-Proxy Companion Self-Signed CA $(date +%s)-$RANDOM" -out /etc/nginx/certs/ca.crt
     fi
 fi
 
